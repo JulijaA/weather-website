225fd03f102430170c317df32210ed36bd5eb4c0
JulijaA <julija.atanasovska13@gmail.com>
2022-07-12 12:24:13 +0200

    changing the style of the widget and removing the future weather forecast

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ const currentTempEl = document.getElementById('temp');
 const currentCountry = document.getElementById('country');
 let iconEl = document.getElementById("icon");
 const weatherForecastEl = document.getElementById('weather-forecast');
-
+const dynamicImg = document.getElementById('dynamicImg');
+const desc = document.getElementById('description');
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 const API_KEY = '20e6b0e3bf8e4abefb9fd54a03f0e429';
+
+const imagesForWeather = {
+    Clear: './img/sunny1.png',
+    Clouds: 'https://images.pexels.com/photos/531767/pexels-photo-531767.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    Thunderstorm: './img/thunderstrom.png',
+    Drizzle: "./img/drizzle.png",
+    Rain: './img/rain1.png',
+    Snow: './img/snow.png',
+    Fog: './img/fog1.png',
+
+}
 
 setInterval(() => {
     const time = new Date();
@@ -24,9 +36,10 @@ setInterval(() => {
 
     timeEl.innerHTML = hoursIn12HrFormat + `:` + (minutes < 10 ? '0' + minutes : minutes) + ' ' + `<span id="am-pm">${ampm}</span>`;
     dateEl.innerHTML = days[day] + ", " + date + ' ' + months[month];
+    getWeatherData();
 }, 1000);
 
-getWeatherData();
+
 function getWeatherData() {
     navigator.geolocation.getCurrentPosition((success) => {
         let { latitude, longitude } = success.coords;
@@ -40,58 +53,74 @@ function getWeatherData() {
 
 function showWeatherData(data) {
     let { humidity, pressure, sunrise, sunset, wind_speed, temp} = data.current;
+    let { main } = data.current.weather[0];
     let { icon } = data.current.weather[0];
     let { description} = data.current.weather[0];
-    currentTempEl.innerHTML = Math.floor(temp) + " " + "&#8451;";
+
+    function dynamicBackground () {
+        for(const key in imagesForWeather) {
+            if(key === main) {
+            dynamicImg.innerHTML = 
+            `
+            <img src="${imagesForWeather[key]}" width='100%';> 
+            `;
+            }
+        }
+    }
+
+    currentTempEl.innerHTML = Math.floor(temp) + "&#8451;";
     currentCountry.innerHTML = data.timezone;
-    iconEl.innerHTML = `
-    <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="" class="w-icon" id="icon">`;
+    desc.innerHTML = description;
+
     currentWeatherItems.innerHTML =
         `
+        <h3>Weather Details</h3>
         <div class="weather-item">
-        <div>Humidity</div>
+        <div class="text-item">Humidity: <i class="fa-solid fa-temperature-half"></i> </div>
         <div>${humidity}%</div>
         
         </div>
         <div class="weather-item">
-        <div>Pressure</div>
+        <div class="text-item" >Pressure: <i class="fa-solid fa-temperature-arrow-up"></i> </div>
             <div>${pressure} mb</div>
         </div>
         <div class="weather-item">
-        <div>Wind Speed</div>
+        <div  class="text-item">Wind: <i class="fa-solid fa-wind"></i> </div>
             <div>${wind_speed} km/h</div>
             
         </div>
         <div class="weather-item">
-        <div>Sunrise</div>
+        <div  class="text-item">Sunrise: <i class="fa-regular fa-sun"></i> </div>
             <div>${window.moment(sunrise * 1000).format('HH:hh a')}</div>
             
         </div>
         <div class="weather-item">
-        <div>Sunset</div>
+        <div class="text-item">Sunset: <i class="fa-regular fa-moon"></i></div>
             <div>${window.moment(sunset * 1000).format('HH:hh a')}</div>
-            
         </div>
     `;
 
-     let otherDayForcast = '';
-    data.daily.forEach((day, idx) => {
-        if(idx == 0){
-            
-        }else {
-            otherDayForcast += `
-            <div class="weather-forecast-item">
-                <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
-                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-                <div class="temp">${Math.floor(day.temp.day)}&#176;C</div>
-            </div>
-            
-            `
-        }
-    })
+       dynamicBackground();
 
+    // =============== Code for showing the forecast for 7 days ===================================
 
-    weatherForecastEl.innerHTML = otherDayForcast;
+    // let otherDayForcast = '';
+    // data.daily.forEach((day, idx) => {
+    //     if(idx == 0){
+            
+    //     }else {
+    //         otherDayForcast += `
+    //         <div class="weather-forecast-item">
+    //             <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+    //             <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+    //             <div class="temp">${Math.floor(day.temp.day)}&#176;C</div>
+    //         </div>
+            
+    //         `
+    //     }
+    // })
+
+    // weatherForecastEl.innerHTML = otherDayForcast;
 }
 
 
